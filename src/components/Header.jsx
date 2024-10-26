4c9a3389a9bffdd2383dbe74120ec5313c13192a
Joaco4200 <joaquinscarone1012@gmail.com>
2024-10-26 00:35:38 -0300

    auth0 library added, login and logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchBar from "../atoms/SearchBar";
 import CreatePostIcon from "../atoms/CreatePostIcon";
 import ProfileIcon from "../atoms/ProfileIcon";
+//import LoginButton from "../atoms/LoginButton";
+//import LogoutButton from "../atoms/LogoutButton";
+
 
 const Header = () => {
 
@@ -11,10 +14,13 @@ const Header = () => {
         //Container main del header
         <div className="d-flex justify-content-between align-items-center p-3 bg-dark" style={{width: '100vw', height: '4vw' }}> 
             
-            <div className="d-flex align-items-center">
-                <img src= "/logoucu.png" alt= "logo" style={{width: '5vw', height: '5vw' }}/>
-                <txt style={{ fontWeight: 'bold', fontFamily: 'Noto Sans', fontSize: '1.6rem', color: '#ffffff' }}>UCUddit</txt>
-            </div>
+            <button className="btn">
+                <div className="d-flex align-items-center">
+                    <img src= "/logoucu.png" alt= "logo" style={{width: '5vw', height: '5vw' }}/>
+                    <txt style={{ fontWeight: 'bold', fontFamily: 'Noto Sans', fontSize: '1.6rem', color: '#ffffff' }}>UCUDDIT</txt>
+                </div>
+            </button>
+            
             
             <SearchBar/>
 
@@ -22,6 +28,7 @@ const Header = () => {
                 <CreatePostIcon/>
                 <ProfileIcon/>
             </div>
+
         
         </div>
     )
